c043b531fb5225d2e59a1584735054403ff87067
fabetabilo <fab.tabilo@duocuc.cl>
2025-06-23 23:29:18 -0400

    Implementacion de variables de entorno, y conexion a base de datos MySQL

--- gestor-tiendas/src/main/java/com/gestor_tiendas/gestor_tiendas/GestorTiendasApplication.java
+++ gestor-tiendas/src/main/java/com/gestor_tiendas/gestor_tiendas/GestorTiendasApplication.java
@@ -1,5 +1,6 @@
 package com.gestor_tiendas.gestor_tiendas;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GestorTiendasApplication {
 
 	public static void main(String[] args) {
+		// carga variables de entorno ANTES que Spring Boot inicie, para que esten disponibles para Spring Boot
+		Dotenv dotenv = Dotenv.load();
+
 		SpringApplication.run(GestorTiendasApplication.class, args);
 	}
 
