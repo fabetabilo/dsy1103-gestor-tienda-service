43a38865c263e0ebdeb5d93219bd7320cb0340bd
fabetabilo <fab.tabilo@duocuc.cl>
2025-07-01 16:56:29 -0400

    1er deploy

--- gestor-tiendas/src/main/java/com/gestor_tiendas/gestor_tiendas/GestorTiendasApplication.java
+++ gestor-tiendas/src/main/java/com/gestor_tiendas/gestor_tiendas/GestorTiendasApplication.java
@@ -1,6 +1,6 @@
 package com.gestor_tiendas.gestor_tiendas;
 
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +9,7 @@ public class GestorTiendasApplication {
 
 	public static void main(String[] args) {
 		// carga variables de entorno ANTES que Spring Boot inicie, para que esten disponibles para Spring Boot
-		Dotenv dotenv = Dotenv.load();
+		//Dotenv dotenv = Dotenv.load();
 
 		SpringApplication.run(GestorTiendasApplication.class, args);
 	}
