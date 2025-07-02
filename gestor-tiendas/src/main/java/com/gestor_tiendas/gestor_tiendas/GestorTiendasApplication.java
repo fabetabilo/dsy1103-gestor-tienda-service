edc6440ef9613a36fc8e65bcf6b61c1cbcff3d09
fabetabilo <fab.tabilo@duocuc.cl>
2025-07-02 00:13:41 -0400

    back

--- gestor-tiendas/src/main/java/com/gestor_tiendas/gestor_tiendas/GestorTiendasApplication.java
+++ gestor-tiendas/src/main/java/com/gestor_tiendas/gestor_tiendas/GestorTiendasApplication.java
@@ -3,7 +3,7 @@ package com.gestor_tiendas.gestor_tiendas;
 //import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class GestorTiendasApplication {
 
