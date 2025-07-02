b57455b17e36dfc73758a3e3eb4b3981d8b708c2
fabetabilo <fab.tabilo@duocuc.cl>
2025-07-01 22:29:22 -0400

    aber

--- gestor-tiendas/src/test/java/com/gestor_tiendas/gestor_tiendas/GestorTiendasApplicationTests.java
+++ gestor-tiendas/src/test/java/com/gestor_tiendas/gestor_tiendas/GestorTiendasApplicationTests.java
@@ -2,8 +2,10 @@ package com.gestor_tiendas.gestor_tiendas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GestorTiendasApplicationTests {
 
 	@Test
